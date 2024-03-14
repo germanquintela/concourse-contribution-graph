3611e7f0b2bd2bc67d28c2f1dd6644a98665d339
germanquintela96@gmail.com <germanquintela96@gmail.com>
2024-03-14 11:52:53 -0300

    refactor: fetch url variable

--- src/services/getContributions.tsx
+++ src/services/getContributions.tsx
@@ -1,6 +1,8 @@
+const COMMIT_ACTIVITY_URL = "https://api.github.com/repos/facebook/react/stats/commit_activity"
+
 export const getContributions = async () => {
   try {
-    const res = await fetch("https://api.github.com/repos/facebook/react/stats/commit_activity");
+    const res = await fetch(COMMIT_ACTIVITY_URL);
     if (!res.ok) {
       throw new Error(`Failed to fetch data, status: ${res.status}`);
     }
